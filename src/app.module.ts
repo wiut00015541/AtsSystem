a56d8df258ab9baf66557e02e02abba2b6c61dbe
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-01-11 14:15:35 +0500

    jobs controller and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JobsModule],
   controllers: [AppController],
   providers: [AppService],
 })
