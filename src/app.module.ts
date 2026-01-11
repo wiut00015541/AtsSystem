e2e1c03c11bf1b2693b42762ef315ba375f6f48c
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-01-11 14:21:00 +0500

    applications service and controller created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { JobsModule } from './jobs/jobs.module';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [UsersModule, JobsModule],
+  imports: [UsersModule, JobsModule, ApplicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
